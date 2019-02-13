bc18cdaf2eee26802445f25749159d9e7a86e516
hyosunjeong <jeong8161@naver.com>
2019-02-13 11:18:37 +0900

    first

---
+++ src/main/java/com/biz/naver/config/NaverClientSample.java
@@ -0,0 +1,12 @@
+package com.biz.naver.config;
+
+public class NaverClientSample {
+
+	/*
+	 * 네이버 오픈 API 사용등록을 실행 한 후 
+	 * 본인의 CLIENT ID와 CLIDNT SECRET를 작성한 후 
+	 * NaverClient로 파일 이름을 변경 한 후 프로젝트를 실행하세요
+	 */
+	public static final String ID = "YOUR CLIENT ID";
+	public static final String KEY = "YOUR CLIENT SECRET";
+}
